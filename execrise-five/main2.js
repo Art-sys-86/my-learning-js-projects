094395ca2819f10bb394a688615e08ce8e561e40
Art-sys-86 <hithukten@gmail.com>
2025-09-11 21:41:35 +0600

    Update main2.js

--- execrise-five/main2.js
+++ execrise-five/main2.js
@@ -1,3 +1,5 @@
+
+
 const textBox = document.getElementById("textBox");
 const toBinary = document.getElementById("toBinary");
 const toDecimal = document.getElementById("toDecimal");
@@ -10,12 +12,12 @@ function convert(){
     if(toBinary.checked){
         time = Number(textBox.value);
         time = time/60;
-        result.textContent = time + "min"
+        result.textContent = time.toFixed(1) + "min"
     }
     else if(toDecimal.checked){
         time = Number(textBox.value);
         time = time/60;
-        result.textContent = time + "hr"
+        result.textContent = time.toFixed(1) + "hr"
     }
     else{
         result.textContent = `Please Select a Unit`;
